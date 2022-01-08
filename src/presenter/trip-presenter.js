231cef830230c07ecb27f40262eeba64ffab81fe
Vladimir Yakovlev <mr.kollc@list.ru>
2022-01-08 15:32:00 +0300

    Added  sort by time, by day, by price

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -10,8 +10,9 @@ import SiteListView from '../view/site-list-view';
 import SiteEmptyView from '../view/site-empty-view';
 import PointPresenter from './point-presenter';
 import {updateItem} from '../utils/common.js';
-
-const POINT_COUNT = 15;
+import { POINT_COUNT } from '../consts';
+import { SortType } from '../consts';
+import { sortByTime, sortByPrice } from '../utils/common.js';
 
 export default class TripPresenter {
   #points = [];
@@ -20,12 +21,16 @@ export default class TripPresenter {
   #tripSectionElement = null;
   #tripListContainerElement = new SiteListView().element;
   #pointPresenter = new Map();
+  #sortComponent = new SiteSortView();
+  #currentSortType = SortType.DEFAULT;
+  #sourcedBoardPoints = [];
 
   constructor(points, navigationContainerElement, filterContainer, tripSectionElement) {
     this.#navigationContainerElement = navigationContainerElement;
     this.#filterContainerElement = filterContainer;
     this.#tripSectionElement = tripSectionElement;
     this.#points = points;
+    this.#sourcedBoardPoints = [...points];
 
     render(this.#navigationContainerElement, new SiteMenuView(), RenderPosition.BEFOREEND);
     render(this.#filterContainerElement, new SiteFilterView(), RenderPosition.BEFOREEND);
@@ -34,13 +39,49 @@ export default class TripPresenter {
   }
 
   #renderTripContainer = () => {
-    render(this.#tripSectionElement, new SiteSortView(), RenderPosition.BEFOREEND);
+    this.#renderSort();
     render(this.#tripSectionElement, this.#tripListContainerElement, RenderPosition.BEFOREEND);
     this.#renderListPoints();
   }
 
+  #clickSortTypeHandler = (sortType) => {
+    if (this.#currentSortType === sortType) {
+      return;
+    }
+
+    this.#sortTasks(sortType);
+    this.#clearListPoints();
+    this.#renderListPoints();
+  }
+
+  #clearListPoints = () => {
+    this.#pointPresenter.forEach((presenter) => presenter.destroy());
+    this.#pointPresenter.clear();
+  }
+
+  #sortTasks = (sortType) => {
+    switch (sortType) {
+      case SortType.BY_TIME:
+        this.#points.sort(sortByTime);
+        break;
+      case SortType.BY_PRICE:
+        this.#points.sort(sortByPrice);
+        break;
+      default:
+        this.#points = [...this.#sourcedBoardPoints];
+    }
+
+    this.#currentSortType = sortType;
+  }
+
+  #renderSort = () => {
+    render(this.#tripSectionElement, this.#sortComponent, RenderPosition.BEFOREEND);
+    this.#sortComponent.setSortTypeChangeHalder(this.#clickSortTypeHandler);
+  }
+
   #handlePointChange = (updatePoint) => {
     this.#points = updateItem(this.#points, updatePoint);
+    this.#sourcedBoardPoints = updateItem(this.#sourcedBoardPoints, updatePoint);
     this.#pointPresenter.get(updatePoint.id).init(updatePoint);
   }
 
--- src/view/site-sort-view.js
+++ src/view/site-sort-view.js
@@ -1,10 +1,13 @@
 import AbstractView from './abstract-view.js';
+import {
+  SortType
+} from '../consts.js';
 
 const createSiteSortTemplate = () => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     <div class="trip-sort__item  trip-sort__item--day">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
-      <label class="trip-sort__btn" for="sort-day">Day</label>
+      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+      <label class="trip-sort__btn" for="sort-day" data-sort-type="${SortType.DEFAULT}">Day</label>
     </div>
 
     <div class="trip-sort__item  trip-sort__item--event">
@@ -14,12 +17,12 @@ const createSiteSortTemplate = () => (
 
     <div class="trip-sort__item  trip-sort__item--time">
       <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-      <label class="trip-sort__btn" for="sort-time">Time</label>
+      <label class="trip-sort__btn" for="sort-time" data-sort-type="${SortType.BY_TIME}">Time</label>
     </div>
 
     <div class="trip-sort__item  trip-sort__item--price">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
-      <label class="trip-sort__btn" for="sort-price">Price</label>
+      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+      <label class="trip-sort__btn" for="sort-price" data-sort-type="${SortType.BY_PRICE}">Price</label>
     </div>
 
     <div class="trip-sort__item  trip-sort__item--offer">
@@ -29,9 +32,34 @@ const createSiteSortTemplate = () => (
   </form>`
 );
 
-
 export default class SiteSortView extends AbstractView {
   get template() {
     return createSiteSortTemplate();
+  }
+
+  setSortTypeChangeHalder = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  }
+
+  #sortTypeChangeHandler = (evt) => {
+    const inputs = this.element.querySelectorAll('.trip-sort__input');
+    const currentInput = evt.target.parentNode.querySelector('.trip-sort__input');
+
+    if (evt.target.tagName !== 'LABEL') {
+      return;
+    }
+
+    evt.preventDefault();
+
+    if (!currentInput.hasAttribute('disabled')) {
+      this._callback.sortTypeChange(evt.target.dataset.sortType);
+      this.#addCheckedAttributeToInput(inputs, currentInput);
+    }
+  }
+
+  #addCheckedAttributeToInput = (inputs, currentInput) => {
+    inputs.forEach((input) => input.removeAttribute('checked'));
+    currentInput.setAttribute('checked', true);
   }
 }
